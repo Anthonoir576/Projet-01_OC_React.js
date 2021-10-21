a64b5892c592d58887cefb6c7865789055e14468
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-10-21 07:06:19 +0200

    mise en forme

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,16 @@
+import '../styles/Banner.css';
+
 
 const Banner = () => {
 
     const title = "La maison jungle";
-    return <h1>{title}</h1>;
+
+
+    return  (<div className="lmj-banner">
+                <h1>{title}</h1>
+            </div>);
 
 };
+
 
 export default Banner
